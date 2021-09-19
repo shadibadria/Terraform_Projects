ae9f9702e1a3d26d0b361ae24eebd1e56f622007
shadibadria <shadibadria@gmail.com>
2021-09-19 15:18:29 +0300

    ec2 automation

--- EC2_Provisioning_Automation/main.tf
+++ EC2_Provisioning_Automation/main.tf
@@ -9,6 +9,8 @@ variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
 variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key_location" {}
 
 # VPC
 resource "aws_vpc" "myapp-vpc" {
@@ -59,7 +61,10 @@ resource "aws_security_group" "myapp-sg" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = [ "${var.my_ip}" ]
+        #cidr_blocks = [ "${var.my_ip}" ]
+        cidr_blocks = [ "0.0.0.0/0" ]
+
+
     }
     ingress {
         from_port = 8080
@@ -75,7 +80,56 @@ resource "aws_security_group" "myapp-sg" {
         cidr_blocks = []
     }
     tags = {
-        Name:"${var.env_prefix}-sg"
+        Name = "${var.env_prefix}-sg"
     }
+}
+
+# EC2 
+
+# image
+data "aws_ami" "latest-amazon-Linux-image" {
+    most_recent = true
+    owners = [ "amazon" ] 
+    filter {
+      name = "name"  
+      values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+}
+
+
+# key pair automation
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = "${file(var.public_key_location)}"
+  
+}
+# ec2 config
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-Linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true 
+    key_name = aws_key_pair.ssh-key.key_name
+   
+
+    tags = {
+        Name ="${var.env_prefix}-server"
+    }
+
+}
+
+# Outputs 
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-Linux-image.id
+  
+}
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
   
 }
