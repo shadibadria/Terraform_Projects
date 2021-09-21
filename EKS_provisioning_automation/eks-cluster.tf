45ea440f58d8b9a8bb37fcf9c02b5fc67fcacdaa
shadi <shadibadria@gmail.com>
2021-09-21 11:28:33 +0300

    kubernetes_provider added

--- EKS_provisioning_automation/eks-cluster.tf
+++ EKS_provisioning_automation/eks-cluster.tf
@@ -1,4 +1,19 @@
 
+provider "kubernetes" {
+ # load_config_file       = "false"
+  host = data.aws_eks_cluster.myapp-cluster.endpoint
+  token = data.aws_eks_cluster_auth.myapp-cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
+}
+
+data "aws_eks_cluster" "myapp-cluster" {
+    name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "myapp-cluster" {
+    name = module.eks.cluster_id
+}
+
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -15,7 +30,6 @@ module "eks" {
   }
   
   # worker nodes
-
   worker_groups = [
       {
           instance_type = "t2.small"
